24801a6826114d44679a684f3ceeed02bd4b99a5
Latra <personadelmonton@gmail.com>
2024-10-19 23:25:15 +0200

    Revert "Portfolio"

--- src/components/CasterPortfolio.js
+++ src/components/CasterPortfolio.js
@@ -1,25 +1,21 @@
-import React, { useState, useEffect, useRef } from 'react';
+// CasterPortfolio.js
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import casters from '../data/casters.json';
-import gamesData from '../data/games.json';
 import groups from '../data/groups.json';
 import CollaborationCard from './CollaborationCard';
 import ClipCarousel from './ClipCarousel';
 
+const gameLogos = {
+  "League of Legends": "/images/game_logos/lol.svg",
+  "Valorant": "/images/game_logos/valorant.svg",
+  "Wild Rift": "/images/game_logos/wild.svg"
+};
+
 const CasterPortfolio = () => {
   const { id } = useParams();
   const caster = casters.find(caster => caster.id === parseInt(id));
-
-  // ID predeterminado para "League of Legends"
-  const defaultGameId = "lol";
-
   const [copied, setCopied] = useState(false);
-  const [selectedGame, setSelectedGame] = useState(defaultGameId);
-  const [selectedColor, setSelectedColor] = useState(() => {
-    const defaultGame = gamesData.find(game => game.id === defaultGameId);
-    return defaultGame ? defaultGame.color : '#000';
-  });
-  const sectionRefs = useRef({});
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -38,29 +34,14 @@ const CasterPortfolio = () => {
       .catch(err => console.error('Error al copiar al portapapeles:', err));
   };
 
-  const handleGameSelection = (gameId) => {
-    // Solo cambiar la selección si el ID es diferente del actual
-    if (gameId !== selectedGame) {
-      setSelectedGame(gameId);
-      const game = gamesData.find(g => g.id === gameId);
-      setSelectedColor(game ? game.color : '#000');
-    }
-  };
-
-  const collaborationGroups = selectedGame && caster.profiles && caster.profiles[selectedGame]?.collaborator.map(collab => {
+  // Filtrar la información de los grupos en los que el caster colabora
+  const collaborationGroups = caster.collaborator.map(collab => {
     const group = groups.find(g => g.id === collab.entity);
     return { ...group, ...collab };
   });
 
-  // Refs para las secciones de clips
-
-  // Función para desplazar a la sección correspondiente
-  const scrollToSection = (sectionName) => {
-    sectionRefs.current[sectionName].scrollIntoView({ behavior: 'smooth' });
-  };
-
   return (
-    <div className="min-h-screen bg-gradient-to-b from-avalanche10 via-fearOrange to-avalanche20 p-10 text-avalancheBlue">
+    <div className="min-h-screen bg-gradient-to-b from-avalanche10 via-fearOrange to-avalanche20 p-10">
       {/* Cabecera */}
       <div className="fixed top-0 left-0 w-full bg-white shadow-md transition-all duration-500 z-50 py-2 border-b">
         <div className="flex justify-between items-center w-full px-4">
@@ -71,7 +52,7 @@ const CasterPortfolio = () => {
               className="w-10 h-10"
             />
           </a>
-          <div className="flex justify-center flex-grow text-2xl md:text-3xl font-akiraBold ">
+          <div className="flex justify-center flex-grow text-2xl md:text-3xl font-akiraBold text-avalancheBlue">
             CONOCE A NUESTROS CASTERS
           </div>
         </div>
@@ -81,13 +62,22 @@ const CasterPortfolio = () => {
       <div className="container mx-auto flex flex-col items-center max-w-screen-lg mt-16 pt-6">
         {/* Tarjeta del caster */}
         <div className="relative bg-white rounded-lg shadow-lg p-8 w-full mb-10 min-h-[300px]">
+          <div className="absolute top-4 right-4 flex space-x-2">
+            {caster.games && caster.games.map((game, index) => (
+              <img
+                key={index}
+                src={gameLogos[game]}
+                alt={`${game} logo`}
+                className="w-12 h-12 md:w-14 md:h-14"
+              />
+            ))}
+          </div>
           <div className="flex flex-col md:flex-row items-start md:items-center">
             <div className="flex-shrink-0 flex flex-col items-center w-32 h-32 md:w-40 md:h-40 mb-4 md:mb-0">
               <img
                 src={`${process.env.PUBLIC_URL}${caster.photo}`}
                 alt={`${caster.name} ${caster.surname}`}
-                className="w-full h-full object-cover rounded-full text-avalancheBlue"
-                style={{ border: `4px solid` }}
+                className="w-full h-full object-cover rounded-full border-4 border-avalancheBlue"
               />
               <div className="flex space-x-4 mt-4">
                 <a href={caster.socialLink} target="_blank" rel="noopener noreferrer">
@@ -100,86 +90,26 @@ const CasterPortfolio = () => {
               {copied && <p className="text-sm text-green-600 mt-2">¡Copiado!</p>}
             </div>
             <div className="md:ml-8 flex flex-col mt-4 md:mt-0">
-              <h1 className="text-4xl font-akiraBold text-avalancheBlue">{caster.name}  {caster.surname}</h1>
+              <h1 className="text-4xl font-akiraBold text-avalancheBlue">{caster.name} {caster.surname}</h1>
               <p className="text-2xl font-akiraRegular text-pureBlack mt-1">"{caster.nickname}"</p>
-              <p className="mt-4 text-lg text-gray-800 font-robotoLight text-justify">
+              <p className="mt-4 text-lg text-gray-800 font-robotoLight text-justify ">
                 {caster.description}
               </p>
             </div>
           </div>
-
-          {/* Logos de juegos */}
-          <div className="flex justify-center mt-8 space-x-4">
-            {caster.profiles && Object.keys(caster.profiles).map((gameId, index) => {
-              const game = gamesData.find(g => g.id === gameId);
-              const isActive = selectedGame === gameId;
-              return (
-                <img
-                  key={index}
-                  src={isActive ? game.image_active : game.image_inactive}
-                  alt={`${gameId} logo`}
-                  className="w-12 h-12 md:w-14 md:h-14 cursor-pointer"
-                  onClick={() => handleGameSelection(gameId)}
-                />
-              );
-            })}
-          </div>
-
-          {/* Nombre del juego seleccionado */}
-          {selectedGame && (
-            <p className="text-center mt-4 text-xl font-akiraRegular" style={{ color: selectedColor }}>
-              {gamesData.find(g => g.id === selectedGame)?.name}
-            </p>
-          )}
-
-
           {/* Sección de Colaboraciones */}
-          {selectedGame && caster.profiles && caster.profiles[selectedGame] && (
-            <>
-              <div className="grid mt-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 w-full">
-                {collaborationGroups && collaborationGroups.map((group, index) => (
-                  <CollaborationCard key={index} group={group} selectedColor={selectedColor} />
-                ))}
-              </div>
-
-              {/* Índice de categorías */}
-              {selectedGame && caster.profiles && caster.profiles[selectedGame] && (
-                <div className="flex justify-center mt-8 space-x-4 mb-8">
-                  {Object.keys(caster.profiles[selectedGame].clips).length > 1 &&
-                    Object.keys(caster.profiles[selectedGame].clips).map((sectionName, index) => (
-                      <React.Fragment key={index}>
-                        <button
-                          onClick={() => scrollToSection(sectionName)}
-                          style={{ color: selectedColor }}
-                          className="text-lg font-akiraRegular hover:underline"
-                        >
-                          {sectionName}
-                        </button>
-                        {index < Object.keys(caster.profiles[selectedGame].clips).length - 1 && (
-                          <span className="text-gray-400">|</span>
-                        )}
-                      </React.Fragment>
-                    ))
-                  }
-                </div>
-              )}
-              {/* Secciones de clips dinámicos */}
-              {Object.entries(caster.profiles[selectedGame].clips).map(([sectionName, clips], index) => (
-                <div
-                  key={index}
-                  ref={(el) => (sectionRefs.current[sectionName] = el)}
-                  className="mt-12"
-                >
-                  <h2 className="text-3xl font-akiraRegular mb-6 text-center" style={{ color: selectedColor }}>
-                    {sectionName}
-                  </h2>
-                  <ClipCarousel clips={clips} selectedGame={selectedGame} />
-                </div>
-              ))}
-            </>
-          )}
-
-          {!selectedGame && <p className="text-center mt-8 text-xl text-gray-700">Selecciona un juego para ver más detalles</p>}
+          <div className="grid mt-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 w-full">
+            {collaborationGroups.map((group, index) => (
+              <CollaborationCard
+                key={index}
+                group={group}
+              />
+            ))}
+          </div>
+          <h2 className="text-3xl font-akiraRegular mt-12 mb-6 text-center text-avalancheBlue">Clips Destacados</h2>
+          
+          {/* Clips Destacados Carousel */}
+          <ClipCarousel clips={caster.clips} />
         </div>
       </div>
     </div>
--- src/components/CollaborationCard.js
+++ src/components/CollaborationCard.js
@@ -1,16 +1,16 @@
 // CollaborationCard.js
 import React from 'react';
 
-const CollaborationCard = ({ group, selectedColor }) => {
+const CollaborationCard = ({ group }) => {
   const handleClick = () => {
     window.open(group.rrss, "_blank", "noopener,noreferrer");
   };
 
   return (
-    <div onClick={handleClick} style={{ color: selectedColor }} className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center cursor-pointer">
+    <div onClick={handleClick} className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center cursor-pointer">
       <div className="flex items-center space-x-4 mb-4">
         <img src={group.logo} alt={`${group.name} logo`} className="w-10 h-10" />
-        <h3 className="text-xl font-semibold " style={{ color: selectedColor }}>{group.name}</h3>
+        <h3 className="text-xl font-semibold">{group.name}</h3>
       </div>
       <p className="text-gray-600 ">
         {group.start} - {group.end ? group.end : "Actualidad"}
