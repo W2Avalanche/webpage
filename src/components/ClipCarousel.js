eca309b2e674018a4ca8c2991c39812d3218f4ba
Latra <personadelmonton@gmail.com>
2024-10-19 23:30:11 +0200

    Revert "Revert "Portfolio""

--- src/components/ClipCarousel.js
+++ src/components/ClipCarousel.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../styles/carousel.css';
 
-const ClipCarousel = ({ clips }) => {
+const ClipCarousel = ({ clips, selectedGame }) => {
+  const [startPosition, setStartPosition] = useState(0);
+
+  // Reinicia la posición al cambiar de juego o cuando los clips cambian
+  useEffect(() => {
+    setStartPosition(0);
+  }, [selectedGame, clips]);
+
   return (
-    <Carousel>
-      {clips.map((clipId, index) => (
-        <div key={index}>
-          <iframe
-            title={`clip-${index}`}
-            src={`https://clips.twitch.tv/embed?clip=${clipId}&parent=localhost`}
-            allowFullScreen
-            className="rounded-lg border-0 border-gray-300 h-96 w-5/6"
-          ></iframe>
-        </div>
-      ))}
+    <Carousel selectedItem={startPosition} onChange={() => setStartPosition(null)}>
+      {clips.map((clip, index) => {
+        const { source, media } = clip;
+        let srcUrl = '';
+
+        // Definir el URL del embed en función del origen del clip
+        if (source === 'twitch') {
+          srcUrl = `https://clips.twitch.tv/embed?clip=${media}&parent=localhost`;
+        } else if (source === 'youtube') {
+          srcUrl = `https://www.youtube.com/embed/${media}`;
+        }
+
+        return (
+          <div key={index}>
+            <iframe
+              title={`clip-${index}`}
+              src={srcUrl}
+              allowFullScreen
+              className="rounded-lg border-0 border-gray-300 h-96 w-5/6"
+            ></iframe>
+          </div>
+        );
+      })}
     </Carousel>
   );
 };
